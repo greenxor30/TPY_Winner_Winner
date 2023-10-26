faae421be92857365890da0d052b39b00882203f
Mechinot Hanich - אופק שנער <xor-hanich-30@greendreamteam.onmicrosoft.com>
2023-10-26 11:14:03 +0300

    add file with the answer

---
+++ Winner_Winner.py
@@ -0,0 +1,7 @@
+n1 = int(input("enter a number"))
+n2 = int(input("enter a number"))
+str_to_print = input("enter a string to print")
+if n1 * n2 > 15:
+    print (str_to_print)
+else:
+    print("chicken dinner")
